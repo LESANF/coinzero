5f16642c752a456a1222e94836d656bb28cf10ad
LESANF <nagong1000@naver.com>
2022-08-12 22:32:13 +0900

    update: CoinRank(Rigth) UI

--- src/Components/CoinRank.tsx
+++ src/Components/CoinRank.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
 import { AiFillCaretUp, AiFillCaretDown } from 'react-icons/ai';
 import { BsQuestionCircle } from 'react-icons/bs';
+import { VscArrowSwap } from 'react-icons/vsc';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 import { getDetailCoin, getMarketCoins, IAssignCoin, ICoin, ICoinDetail } from '../Api/coinInfo';
@@ -37,6 +38,7 @@ const HeaderTitle = styled.h2`
         height: 23px;
         margin-right: 8px;
         background-image: url('https://coinone.co.kr/landing/assets/images/main/coin-ranking.svg');
+        //background-image: url('image/CoinRank.png');
     }
 `;
 
@@ -56,7 +58,7 @@ const LinkLogo = styled.span`
 //Rank Box
 const RankBox = styled.div`
     margin-top: 19px;
-    padding: 8px 7px;
+    padding: 16px;
     border-radius: 4px;
     border: 1px solid #e4e5e8;
     box-shadow: 0 3px 10px 0 rgba(66, 66, 66, 0.05);
@@ -66,7 +68,6 @@ const ItemList = styled.ul``;
 const ListHeader = styled.li`
     color: #aeb3bb;
     display: flex;
-    //justify-content: space-around;
     margin-bottom: 15px;
 `;
 
@@ -88,15 +89,21 @@ const SignSymbol = styled.span`
 const SignTooltip = styled.div``;
 
 //현재가
-const CurPriceHead = styled.div``;
+const CurPriceHead = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 120px;
+`;
 
 const CurPriceBtn = styled.button`
     display: flex;
+    cursor: pointer;
     align-items: center;
     border: none;
     justify-content: flex-end;
     padding: 0 10px;
-    background-color: rgba(72, 77, 85, 0.05);
+    background-color: transparent;
+    //background-color: rgba(72, 77, 85, 0.05);
 `;
 
 const CurText = styled.span`
@@ -111,15 +118,22 @@ const CurSortSymbol = styled.div`
 `;
 
 //등락률
-const UpDownRateHead = styled.div``;
+const UpDownRateHead = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 72px;
+    margin-left: 18px;
+`;
 
 const UpDownRateBtn = styled.button`
+    cursor: pointer;
     display: flex;
     align-items: center;
     border: none;
     justify-content: flex-end;
     padding: 0 10px;
-    background-color: rgba(72, 77, 85, 0.05);
+    background-color: transparent;
+    //background-color: rgba(72, 77, 85, 0.05);
 `;
 
 const UpDownText = styled.span`
@@ -133,15 +147,21 @@ const UpdownSymbol = styled.div`
     justify-content: flex-start;
 `;
 
-//거래량
-const TradePrice = styled.div``;
+//거래대금
+const TradePriceHead = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 125px;
+`;
 
 const TradePriceBtn = styled.button`
+    cursor: pointer;
     display: flex;
     align-items: center;
     border: none;
     justify-content: flex-end;
     padding: 0 10px;
+    //background-color: transparent;
     background-color: rgba(72, 77, 85, 0.05);
 `;
 
@@ -159,19 +179,102 @@ const TradeSymbol = styled.div`
 //Coin Rank List
 const List = styled.li`
     display: flex;
-    justify-content: space-around;
+    margin-bottom: 20px;
+    &:last-child {
+        margin-bottom: 0;
+    }
+    border-bottom: 1px solid #f8f8f9;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: rgb(248, 248, 249);
 `;
 
 //Symbol and Name(eng, kor)
 const SignStandard = styled.div`
     display: flex;
+    align-items: center;
+    width: 220px;
 `;
 
-const SignMark = styled.div``;
+const SignMark = styled.div`
+    margin-right: 12px;
+    width: 38px;
+    height: 38px;
+`;
 
 const NameBox = styled.div`
     display: flex;
     flex-direction: column;
+    justify-content: center;
+`;
+
+const NameEng = styled.span`
+    margin-bottom: 7px;
+    font-size: 14px;
+    font-weight: 700;
+    color: #18191c;
+    text-transform: uppercase;
+`;
+const NameKor = styled.span`
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    max-width: 103px;
+    font-size: 10px;
+    color: #79818f;
+`;
+
+//Current Coin Pirce
+const Price = styled.div`
+    width: 120px;
+    text-align: right;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+const CoinPrice = styled.span<{ upDownColor: string }>`
+    font-size: 15px;
+    text-align: right;
+    color: ${(props) =>
+        props.upDownColor === 'RISE' ? '#e12343' : props.upDownColor === 'FALL' ? '#1763b6' : 'black'};
+`;
+
+//UpDown Rate
+const UpDownRate = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 72px;
+    margin-left: 18px;
+`;
+
+const CoinUpDown = styled.span<{ upDownColor: string; positiveChk: boolean }>`
+    width: 80px;
+    font-size: 13px;
+    font-weight: 700;
+    text-align: right;
+    color: ${(props) =>
+        props.upDownColor === 'RISE' ? '#e12343' : props.upDownColor === 'FALL' ? '#1763b6' : 'black'};
+    &:after {
+        content: ' %';
+    }
+    &:before {
+        content: '${(props) => (props.positiveChk ? '+' : '')}';
+    }
+`;
+
+//거래대금
+const TradeAmount = styled.div`
+    width: 125px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+`;
+
+const TradeAcc = styled.span`
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    color: #79818f;
 `;
 
 function CoinRank() {
@@ -253,9 +356,9 @@ function CoinRank() {
                                     </UpdownSymbol>
                                 </UpDownRateBtn>
                             </UpDownRateHead>
-                            <TradePrice>
+                            <TradePriceHead>
                                 <TradePriceBtn>
-                                    <TradeText>거래량</TradeText>
+                                    <TradeText>거래대금</TradeText>
                                     <TradeSymbol>
                                         <span>
                                             <AiFillCaretUp />
@@ -265,18 +368,64 @@ function CoinRank() {
                                         </span>
                                     </TradeSymbol>
                                 </TradePriceBtn>
-                            </TradePrice>
+                            </TradePriceHead>
                         </ListHeader>
                         {combineObj && combineObj.length > 0 ? (
                             combineObj.map((v, i) => (
                                 //세가지로 랭크를 매겨야하는데 (현재가격(최대, 최소)-default / 등락률(최대, 최소) / 거래대금(최대, 최소))
                                 <List key={i}>
-                                    {v.market} / {Math.floor(v.trade_price).toLocaleString('ko-KR')} /{' '}
-                                    {v.korean_name} / {v.acc_trade_price_24h} /{' '}
-                                    {(
-                                        ((v.trade_price - v.prev_closing_price) / v.prev_closing_price) *
-                                        100
-                                    ).toFixed(2)}
+                                    <SignStandard>
+                                        <SignMark>
+                                            <img
+                                                src={`https://static.upbit.com/logos/${v.market
+                                                    .split('-')[1]
+                                                    .toUpperCase()}.png`}
+                                                alt={v.market.split('-')[1]}
+                                                title={v.market.split('-')[1]}
+                                                style={{
+                                                    height: '38px',
+                                                    width: '38px',
+                                                    // boxShadow: '0 3px 10px 0 rgba(66, 66, 66, 0.05)',
+                                                    // borderRadius: '7px',
+                                                }}
+                                            />
+                                        </SignMark>
+                                        <NameBox>
+                                            <NameEng>{v.market.split('-')[1]}</NameEng>
+                                            <NameKor>{v.korean_name}</NameKor>
+                                        </NameBox>
+                                    </SignStandard>
+                                    <Price>
+                                        <CoinPrice upDownColor={v.change}>
+                                            {v.trade_price.toLocaleString('ko-KR')}
+                                        </CoinPrice>
+                                    </Price>
+                                    <UpDownRate>
+                                        <CoinUpDown
+                                            upDownColor={v.change}
+                                            positiveChk={
+                                                ((v.trade_price - v.prev_closing_price) /
+                                                    v.prev_closing_price) *
+                                                    100 >
+                                                0
+                                            }
+                                        >
+                                            {(
+                                                ((v.trade_price - v.prev_closing_price) /
+                                                    v.prev_closing_price) *
+                                                100
+                                            ).toFixed(2)}
+                                        </CoinUpDown>
+                                    </UpDownRate>
+                                    <TradeAmount>
+                                        <TradeAcc>
+                                            {`${Math.floor(
+                                                Math.floor(v.acc_trade_price_24h) / 1000000
+                                            ).toLocaleString('ko-KR')}백만`}
+                                            &nbsp;&nbsp;
+                                            <VscArrowSwap />
+                                        </TradeAcc>
+                                    </TradeAmount>
                                 </List>
                             ))
                         ) : (
