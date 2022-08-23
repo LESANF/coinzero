b36d3f7c51f696e2803ac2cfadabca0e14bd6c17
LESANF <nagong1000@naver.com>
2022-08-23 21:54:41 +0900

    add: Vertical Notice Slider

--- src/Components/VerticalNotice.tsx
+++ src/Components/VerticalNotice.tsx
@@ -1,5 +1,40 @@
+import styled from 'styled-components';
+import { Autoplay } from 'swiper';
+import { SwiperModule } from 'swiper/types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+const Frame = styled.div`
+    margin-top: 25px;
+    height: 60px;
+    border: 2px solid red;
+`;
+
+const swiperSetting: SwiperModule = {
+    modules: [Autoplay],
+    direction: 'vertical',
+    loop: true,
+    autoplay: { delay: 4000, disableOnInteraction: false },
+    speed: 550,
+};
+
+const swiperStyle = {
+    height: '60px',
+    backgroundColor: 'peru',
+};
+
 function VerticalNotice() {
-    return <h1>VerticalNotice</h1>;
+    return (
+        <Frame>
+            {swiperSetting && (
+                <Swiper {...swiperSetting} style={swiperStyle}>
+                    <SwiperSlide>1</SwiperSlide>
+                    <SwiperSlide>2</SwiperSlide>
+                    <SwiperSlide>3</SwiperSlide>
+                    <SwiperSlide>4</SwiperSlide>
+                </Swiper>
+            )}
+        </Frame>
+    );
 }
 
 export default VerticalNotice;
