251e4d6d995dc679f0f4e886627b8d63423392e6
LESANF <nagong1000@naver.com>
2022-08-30 02:19:35 +0900

    add: VerticalNotice ScrollTo && dayjs

--- src/Components/ButtonScroll.tsx
+++ src/Components/ButtonScroll.tsx
@@ -1,6 +1,8 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import _ from 'lodash';
+import dayjs from 'dayjs';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
 
 const Frame = styled.div`
     /* height: 232px;
@@ -8,13 +10,52 @@ const Frame = styled.div`
     box-shadow: 0 3px 10px 0 rgba(66, 66, 66, 0.05);
     border: 1px solid #e4e5e8;
     grid-area: item4;
-    /* overflow: hidden; */
+    overflow: hidden;
 `;
+
+//Title, Button
 const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     box-sizing: border-box;
     padding: 12px 12px 3px 12px;
     height: 41px;
 `;
+
+const ListTitle = styled.h2`
+    font-size: 12px;
+    font-weight: 700;
+    color: #aeb3bb;
+`;
+
+const ListDay = styled.span`
+    font-size: 10px;
+    margin-left: 5;
+`;
+
+//Up, Down Btn
+const BtnBox = styled.div`
+    display: flex;
+    button:first-child {
+        margin-right: 2px;
+    }
+`;
+
+const ListBtn = styled.button<{ curPosition: number; upDownChk: boolean }>`
+    //upDownChk: true(Up), false(Down)
+    background-color: transparent;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 26px;
+    height: 26px;
+    border-radius: 2px;
+    border: 1px solid #e4e5e8;
+    color: #aeb3bb;
+    font-size: 14px;
+`;
+
 const Content = styled.div`
     height: 215px;
     overflow-x: hidden;
@@ -38,6 +79,7 @@ const Content = styled.div`
     }
 `;
 
+//ItemList
 const Items = styled.ul`
     padding: 0 12px 12px;
     overflow: hidden;
@@ -48,17 +90,45 @@ const Items = styled.ul`
 `;
 
 const Item = styled.li`
+    display: flex;
+    flex-direction: column;
     box-sizing: border-box;
+    justify-content: center;
     margin-bottom: 5px;
     width: 100%;
     height: 66px;
-    background-color: red;
-    //background-color: #f8f8f9;
+    background-color: #f8f8f9;
     border-radius: 8px;
-    padding: 12px;
+    padding: 8px 10px;
 `;
 
+const ItemTitle = styled.span`
+    font-size: 10px;
+    color: #7f8794;
+`;
+const ItemContent = styled.span`
+    margin-top: 12px;
+    font-size: 12px;
+    color: #484d55;
+    word-break: keep-all;
+`;
+
+const AddSummary = styled.p`
+    /* overflow: hidden;
+    height: 0;
+    opacity: 0;
+    transition: opacity 0.2s, height 0.2s, padding 0.2s; */
+
+    color: #aeb3bb;
+    font-size: 10px;
+    height: auto;
+    padding-top: 4px;
+    opacity: 1;
+`;
+
+//Footer Sign
 const FooterSign = styled.p`
+    margin-bottom: 10px;
     text-align: center;
     font-size: 12px;
     font-weight: 700;
@@ -67,14 +137,21 @@ const FooterSign = styled.p`
 `;
 
 function ButtonScroll() {
+    const [curScrollPos, setCurScrollPos] = useState<number>(0);
+
+    //get Date
+    const now = dayjs();
+
+    //scroll event
     const contentScrl = useRef<HTMLDivElement | null>(null);
     // const scrollDiv = _.throttle((e: React.UIEvent<HTMLDivElement>) => {
     //     console.log(contentScrl.current?.scrollTop);
     // }, 0);
 
     const listScrollUp = () => {
-        contentScrl.current?.scrollTo({ top: 10, behavior: 'smooth' });
+        // if(contentScrl.current?.scrollTop > 0);
         console.log(contentScrl.current?.scrollTop);
+        contentScrl.current?.scrollTo({ top: 100, behavior: 'smooth' });
     };
     const listScrollDown = () => {
         contentScrl.current?.scrollTo({ top: 90, behavior: 'smooth' });
@@ -84,14 +161,29 @@ function ButtonScroll() {
     return (
         <Frame>
             <Header>
-                헤더 버튼 포함
-                <button onClick={listScrollUp}>up</button>
-                <button onClick={listScrollDown}>down</button>
+                <ListTitle>
+                    Coinnews <ListDay>{now.format('YYYY-MM-DD')}</ListDay>
+                </ListTitle>
+                <BtnBox>
+                    <ListBtn curPosition={curScrollPos} upDownChk={true} onClick={listScrollUp}>
+                        <IoIosArrowUp />
+                    </ListBtn>
+                    <ListBtn curPosition={curScrollPos} upDownChk={false} onClick={listScrollDown}>
+                        <IoIosArrowDown />
+                    </ListBtn>
+                </BtnBox>
             </Header>
             <Content ref={contentScrl}>
                 <Items>
                     {[1, 2, 3, 4, 5].map((v, i) => (
-                        <Item key={i}>{v}</Item>
+                        <Item key={i}>
+                            <ItemTitle>51분전</ItemTitle>
+                            <ItemContent>FTX CEO "후오비 인수 계획 전혀 없다"</ItemContent>
+                            <AddSummary>
+                                메타가 공식 트위터를 통해 인스타그램에 이어 페이스북의 NFT 지원 서비스를
+                                실시한다고 발표했다.
+                            </AddSummary>
+                        </Item>
                     ))}
                 </Items>
                 <FooterSign>MADE BY LESA</FooterSign>
